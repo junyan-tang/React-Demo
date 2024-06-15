b6db37b5796c59346373bf02f3d2857c93947c5e
Junyan Tang <junyan.tang@duke.edu>
2024-06-15 19:21:50 +0800

    call-back function and hooks

---
+++ react-demo/src/Home.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import BlogList from './BlogList';
+
+const Home = () => {
+    //let name = 'mario'
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+    const handleClick = () => {
+       setName('max');
+    }
+    const [blogs, setBlogs] = useState([
+            { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+            { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+            { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        ]);
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter( (blog) => blog.id !== id)
+        setBlogs(newBlogs);
+    }
+    return (  
+        <div className="home">
+            <BlogList blogs={blogs} title = 'All blogs' handleDelete={handleDelete} />
+            <BlogList blogs =  {blogs.filter((blog)=> blog.author === 'mario')} title = "Marios's blogs" />
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ react-demo/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>dojo blog</h1>
+            <div className="links">
+                <a href="/">dojo blog</a>
+                <a href="/create" >New blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
