95a8dd6734f991b683d220791c53bbf791dcb295
Junyan Tang <junyan.tang@duke.edu>
2024-06-15 20:38:07 +0800

    fetch from server

--- react-demo/src/BlogList.js
+++ react-demo/src/BlogList.js
@@ -1,11 +1,13 @@
-const BlogList = (props) => {
+const BlogList = ( {blogs, title, handleDelete}) => {
     const blogs = props.blogs;
+    
     return (  
         <div className="blog-list">
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
+                    <button onClick={()=> handleDelete(blog.idf)}>delete blog</button>
                 </div>
             ))}
         </div>
--- react-demo/src/Home.js
+++ react-demo/src/Home.js
@@ -1,26 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
     //let name = 'mario'
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-    const handleClick = () => {
-       setName('max');
-    }
-    const [blogs, setBlogs] = useState([
-            { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-            { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-            { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-        ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    
     const handleDelete = (id) => {
         const newBlogs = blogs.filter( (blog) => blog.id !== id)
         setBlogs(newBlogs);
     }
+    useEffect( () => {
+        fetch('http://localhost:8000/blog')
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("could not fetch data")
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }, []);
+
     return (  
         <div className="home">
-            <BlogList blogs={blogs} title = 'All blogs' handleDelete={handleDelete} />
-            <BlogList blogs =  {blogs.filter((blog)=> blog.author === 'mario')} title = "Marios's blogs" />
+            { isPending && <div> Loading.. </div> }
+            { blogs && <BlogList blogs={blogs} title = 'All blogs' handleDelete={handleDelete} /> }
         </div>
     );
 }
